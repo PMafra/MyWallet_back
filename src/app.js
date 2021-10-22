3a9ed25a80641aeaa739a21620b9a078ea3a608c
PMafra <pmafrarm@gmail.com>
2021-10-22 13:38:14 -0300

    Using bcrypt lib to encrypt password; sign-in and sign-up routes edited for hashed password

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import Joi from "joi";
+import bcrypt from "bcrypt";
 
 import connection from "./database/database.js";
 
@@ -28,11 +29,13 @@ app.post('/sign-up', async (req, res) => {
             return res.status(403).send(`${email} is already registered!`);
         }
 
+        const hashedPassword = bcrypt.hashSync(password, 11);
+
         await connection.query(`
             INSERT INTO users
             (name, email, password, online)
             VALUES ($1, $2, $3, $4);
-        `,[name, email, password, false]);
+        `,[name, email, hashedPassword, false]);
 
         res.sendStatus(201);
     } catch (err) {
@@ -51,7 +54,7 @@ app.post('/sign-in', async (req, res) => {
     try {
     
         const isEmailRegistered = await connection.query(`
-            SELECT email FROM users
+            SELECT * FROM users
             WHERE email = $1 
         `,[email]);
 
@@ -59,13 +62,9 @@ app.post('/sign-in', async (req, res) => {
             return res.status(404).send(`${email} is not registered!`);
         }
 
-        const isPasswordCorrect = await connection.query(`
-            SELECT * FROM users
-            WHERE email = $1 
-            AND password = $2;
-        `,[email, password]);
+        const user = isEmailRegistered.rows[0];
 
-        if(isPasswordCorrect.rowCount === 0) {
+        if (!bcrypt.compareSync(password, user.password)) {
             return res.status(404).send(`The password is wrong!`);
         }
 
