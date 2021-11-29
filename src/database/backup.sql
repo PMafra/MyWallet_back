8f6ab817b0ce08ec4998ec36c79b7cf8d950eddb
PMafra <pmafrarm@gmail.com>
2021-11-29 09:38:04 -0300

    Create new database backup sql file, delete dump sql

---
+++ src/database/backup.sql
@@ -0,0 +1,27 @@
+CREATE TABLE "users" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "sessions" (
+	"id" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"token" TEXT NOT NULL,
+	CONSTRAINT "sessions_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+CREATE TABLE "records" (
+	"id" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"records" TEXT NOT NULL,
+	CONSTRAINT "records_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+ALTER TABLE "records" ADD CONSTRAINT "records_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
